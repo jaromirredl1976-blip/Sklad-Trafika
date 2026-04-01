f4b153c8a9c191d99404fb1f2852d44dfeee244a
jaromirredl1976-blip <jaromirredl1976@gmail.com>
2026-04-01 22:46:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// 🔥 PWA - service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(() => console.log('SW registered'))
+      .catch(err => console.log('SW error', err))
+  })
+}
